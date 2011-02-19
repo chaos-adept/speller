61f64fd6f6eeeb90b6f6c6bb7465bbbf571df028
Denis Rykovanov <denis.rykovanov@gmail.com>
2011-02-19 14:03:26 +0700

    added mock check text

---
+++ SpellChecker/src/com/digitalchaos/spell/Speller.java
@@ -0,0 +1,9 @@
+package com.digitalchaos.spell;
+
+public class Speller 
+{
+	 public boolean checkText(String word)
+	 {
+		 return false;
+	 }
+}
---
+++ TestSpellChecker/src/tests/SpellerTestCase.java
@@ -0,0 +1,26 @@
+package tests;
+
+import junit.framework.TestCase;
+
+public class SpellerTestCase extends TestCase {
+
+	
+	Speller speller;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void testCorrectlyWord()
+	{
+		fail();
+	}
+	
+	public void testWrongWord()
+	{
+		fail();
+	}
+	
+	
+}
